95b09fcf39fc8482596d4bbc94f55470468d2847
Dinesh Thorat <85392842+DineshThorat@users.noreply.github.com>
2025-03-12 12:14:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Fetch the secret value from the environment variable
-secret_key = os.getenv('in-pod-expose')
-
-
 @app.route('/')
 def home():
-    return f"Secret Key: {secret_key}"
+    return "Hello, World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
-
